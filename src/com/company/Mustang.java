f108fcd501eda60220a4f22a1f056f06026639c2
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-11-19 08:44:08 -0500

    Create Mustang.java

---
+++ src/com/company/Mustang.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Mustang {
+
+    public String toString(){
+        String output = "";
+        output += this.getName() +","+" weighs: "+ this.getWeight();
+        return output;
+    }
+
+    private void getName() {
+
+    }
+}
